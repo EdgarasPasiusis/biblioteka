f4f0c94eacbec82486fa6909ae269df6ba9a59e3
Edgaras Pasiusis <edgaras.pasiusis@stud.smk.lt>
2025-09-30 16:02:10 +0300

    add validation for signup and login

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,12 +6,15 @@ const {
   logout,
   login,
   getAuthenticatedUser,
-  protect
+  protect,
 } = require("../controllers/authController");
+const { validateSignup } = require("../validators/signup");
+const { validateLogin } = require("../validators/login");
+const handleValidationErrors = require("../validators/validationResult");
 
-router.route("/signup").post(signup);
+router.route("/signup").post(validateSignup, handleValidationErrors, signup);
 router.route("/logout").get(logout);
-router.route("/login").post(login);
+router.route("/login").post(validateLogin, handleValidationErrors, login);
 router.route("/me").get(protect, getAuthenticatedUser);
 
 module.exports = router;
